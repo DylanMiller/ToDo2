278ec09b01826c301ad8ad41be62094960528a1e
dylanmiller <dmiller3199@gmail.com>
2015-04-24 08:37:08 -0700

    video 10, the last push was actually video 9

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 
 					if (new_task != '') {
 						$.post('includesl/add-task.php', {task: new_task), function(data) {
-							$(('add-new-task input[name=new-task']).val();
+							$('add-new-task input[name=new-task]').val();
 							$(data).appendTo('task-list ul').hide().fadeIn();)
 						});
 					};
