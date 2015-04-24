e0f70bfdd118cd218a071329c7e25ca01ce795a3
dylanmiller <dmiller3199@gmail.com>
2015-04-24 07:46:48 -0700

    video 8

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+		<head>
+			<title>To Do list</title>
+			<link rel="stylesheet" type="text/css" href="css/main.css">
+		</head>
+		<body>
+			<div class="wrap">
+				<div class="task-list">
+					<ul>
+						<?php require("includes/connect.php"); 
+
+						?>
+					</ul>
+				</div>	
+			<form class="add-new-task" autocomplete="off">
+				<input type="text" name="new-task" placeholder="add new item..."/>
+			</form>
+			</div>
+		</body>
+		<script type="text/javascript">
+			add_task();
+
+			function add_task(){
+				$('.add-new-task').submit(function(
+					var new_task = $('.add-new-task input[name=new-task').val();
+
+					if (new_task != '') {
+						$.post('includesl/add-task.php', {task: new_task), function(data) {
+							$(('add-new-task input[name=new-task']).val();
+							$(data).appendTo('task-list ul').hide().fadeIn();)
+						});
+					};
+					return false;
+				));
+			}
+		</script>
+</html>
